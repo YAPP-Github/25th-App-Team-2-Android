ddcfd9c3083197d57ab51427d04b460dd910093e
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2025-01-01 22:57:00 +0900

    [TNT-66] chore: data 모듈 최초 생성

---
+++ data/build.gradle.kts
@@ -0,0 +1,23 @@
+import co.kr.tnt.setNamespace
+
+plugins {
+    id("tnt.android.library")
+    id("tnt.android.hilt")
+    id("kotlinx-serialization")
+}
+
+android {
+    setNamespace("data")
+
+    buildFeatures {
+        buildConfig = true
+    }
+}
+
+dependencies {
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.kotlin.serialization)
+    implementation(libs.okhttp.logging)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.kotlinx.immutable)
+}
