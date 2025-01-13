88e7b0d915697f3e1034d2df0c554438abcc41f8
SeonJeong <sjkim5621@naver.com>
2025-01-13 19:57:36 +0900

    [TNT-85] feat: single button popup dialog 구현

--- core/designsystem/src/main/java/co/kr/tnt/designsystem/component/Dialog.kt
+++ core/designsystem/src/main/java/co/kr/tnt/designsystem/component/Dialog.kt
@@ -3,7 +3,10 @@ package co.kr.tnt.designsystem.component
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
@@ -41,7 +44,8 @@ fun TnTPopupDialog(
             shape = RoundedCornerShape(16.dp),
             colors = cardColors().copy(TnTTheme.colors.commonColors.Common0),
             modifier = modifier
-                .wrapContentSize()
+                .fillMaxWidth()
+                .wrapContentHeight()
                 .padding(horizontal = 32.dp),
         ) {
             Column(
@@ -91,6 +95,65 @@ fun TnTPopupDialog(
     }
 }
 
+@Composable
+fun TnTSingleButtonPopupDialog(
+    title: String,
+    content: String,
+    buttonText: String,
+    modifier: Modifier = Modifier,
+    onButtonClick: () -> Unit,
+    onDismiss: () -> Unit,
+) {
+    Dialog(
+        onDismissRequest = { onDismiss() },
+        properties = DialogProperties(usePlatformDefaultWidth = false),
+    ) {
+        Card(
+            shape = RoundedCornerShape(16.dp),
+            colors = cardColors().copy(TnTTheme.colors.commonColors.Common0),
+            modifier = modifier
+                .wrapContentSize()
+                .padding(horizontal = 32.dp),
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(20.dp),
+            ) {
+                Text(
+                    text = title,
+                    style = TnTTheme.typography.h4.copy(
+                        textAlign = TextAlign.Center,
+                    ),
+                    color = TnTTheme.colors.neutralColors.Neutral900,
+                    modifier = Modifier.padding(top = 20.dp),
+                )
+
+                Text(
+                    text = content,
+                    style = TnTTheme.typography.body2Medium.copy(
+                        textAlign = TextAlign.Center,
+                    ),
+                    color = TnTTheme.colors.neutralColors.Neutral500,
+                    modifier = Modifier.padding(top = 10.dp),
+                )
+
+                TnTTextButton(
+                    size = ButtonSize.Medium,
+                    type = ButtonType.Gray,
+                    text = buttonText,
+                    onClick = onButtonClick,
+                    modifier = Modifier
+                        .defaultMinSize(minWidth = 144.dp)
+                        .padding(top = 20.dp),
+                )
+            }
+        }
+    }
+}
+
 @Preview
 @Composable
 private fun TnTPopupDialogPreview() {
@@ -102,6 +165,20 @@ private fun TnTPopupDialogPreview() {
             rightButtonText = "확인",
             onLeftButtonClick = { },
             onRightButtonClick = { },
+            onDismiss = { },
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun TnTSingleButtonPopupDialogPreview() {
+    TnTTheme {
+        TnTSingleButtonPopupDialog(
+            title = "프로필 사진 설정을 위해\n사진 접근 권한이 필요해요",
+            content = "사진 추가는 프로필 말고도\n운동과 식단 기록에도 사용돼요",
+            buttonText = "닫기",
+            onButtonClick = { },
             onDismiss = { },
         )
     }
