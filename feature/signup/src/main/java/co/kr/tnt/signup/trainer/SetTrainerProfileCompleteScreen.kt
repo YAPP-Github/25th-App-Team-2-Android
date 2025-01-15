fd1b61fe7143d1bf79b9da65cf15a7e3b9913e71
SeonJeongk <sjkim5621@naver.com>
2025-01-16 02:32:27 +0900

    [TNT-110] fix: Lint 위반 import 순서 변경

--- feature/signup/src/main/java/co/kr/tnt/signup/trainer/SetTrainerProfileCompleteScreen.kt
+++ feature/signup/src/main/java/co/kr/tnt/signup/trainer/SetTrainerProfileCompleteScreen.kt
@@ -20,8 +20,8 @@ import androidx.compose.ui.text.style.TextAlign.Companion.Center
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import co.kr.tnt.core.designsystem.R
-import co.kr.tnt.designsystem.theme.TnTTheme
 import co.kr.tnt.designsystem.component.button.TnTBottomButton
+import co.kr.tnt.designsystem.theme.TnTTheme
 
 @Composable
 fun SetTrainerProfileCompleteScreen(
