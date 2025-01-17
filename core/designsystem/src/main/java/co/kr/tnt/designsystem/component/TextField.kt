1273b3bfa10ff7f58472ecfbc2c1f58cf0a36900
SeonJeongk <sjkim5621@naver.com>
2025-01-18 05:49:00 +0900

    feat: 트레이너 코드 입력 구현

--- core/designsystem/src/main/java/co/kr/tnt/designsystem/component/TextField.kt
+++ core/designsystem/src/main/java/co/kr/tnt/designsystem/component/TextField.kt
@@ -182,6 +182,113 @@ fun TnTLabeledTextField(
 }
 
 @Composable
+fun TnTLabeledTextFieldForCode(
+    title: String,
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    placeholder: String? = null,
+    isSingleLine: Boolean = false,
+    showWarning: Boolean = false,
+    isRequired: Boolean = false,
+    warningMessage: String? = null,
+    keyboardType: KeyboardType = KeyboardType.Text,
+    trailingComponent: @Composable BoxScope.() -> Unit = {},
+) {
+    Column(modifier = modifier.fillMaxWidth()) {
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .padding(bottom = 8.dp, end = 4.dp)
+                .fillMaxWidth(),
+        ) {
+            Text(
+                text = title,
+                style = TnTTheme.typography.body1Bold,
+                color = TnTTheme.colors.neutralColors.Neutral900,
+            )
+            if (isRequired) {
+                Text(
+                    text = "*",
+                    style = TnTTheme.typography.body1Bold,
+                    color = TnTTheme.colors.mainColors.Red500,
+                )
+            }
+            Spacer(Modifier.weight(1f))
+        }
+
+        var isFocused by remember { mutableStateOf(false) }
+
+        val lineColor = when {
+            showWarning -> TnTTheme.colors.blueColors.Blue500
+            isFocused -> TnTTheme.colors.neutralColors.Neutral600
+            else -> TnTTheme.colors.neutralColors.Neutral200
+        }
+
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                BasicTextField(
+                    value = value,
+                    onValueChange = onValueChange,
+                    singleLine = isSingleLine,
+                    cursorBrush = SolidColor(TnTTheme.colors.neutralColors.Neutral900),
+                    textStyle = TnTTheme.typography.body1Medium.copy(
+                        color = TnTTheme.colors.neutralColors.Neutral600,
+                    ),
+                    modifier = Modifier
+                        .weight(1f)
+                        .onFocusChanged { focusState ->
+                            isFocused = focusState.isFocused
+                        }
+                        .padding(8.dp),
+                    decorationBox = { innerTextField ->
+                        if (value.isEmpty() && placeholder != null) {
+                            Text(
+                                text = placeholder,
+                                style = TnTTheme.typography.body1Medium,
+                                color = TnTTheme.colors.neutralColors.Neutral400,
+                            )
+                        }
+                        innerTextField()
+                    },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = keyboardType,
+                    ),
+                )
+
+                Box(
+                    modifier = Modifier
+                        .wrapContentSize(Alignment.Center)
+                        .align(Alignment.CenterVertically)
+                        .padding(vertical = 4.dp),
+                    content = trailingComponent,
+                )
+            }
+
+            HorizontalDivider(
+                thickness = 1.dp,
+                color = lineColor,
+            )
+
+            if (showWarning && !warningMessage.isNullOrEmpty()) {
+                Text(
+                    text = warningMessage,
+                    style = TnTTheme.typography.body2Medium,
+                    color = TnTTheme.colors.blueColors.Blue500,
+                    modifier = Modifier.padding(top = 6.dp),
+                )
+            }
+        }
+    }
+}
+
+@Composable
 fun TnTOutlinedTextField(
     value: String,
     onValueChange: (String) -> Unit,
@@ -307,6 +414,39 @@ private fun TnTLabeledTextFieldPreview() {
             isSingleLine = true,
             isRequired = false,
             warningMessage = "${maxLength}자 이내로 입력해주세요",
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp),
+            trailingComponent = {
+                TnTTextButton(
+                    text = "텍스트",
+                    size = ButtonSize.Small,
+                    onClick = { },
+                )
+            },
+        )
+    }
+}
+
+@Preview(showBackground = true, heightDp = 120)
+@Composable
+private fun TnTLabeledTextFieldForCodePreview() {
+    TnTTheme {
+        val maxLength = 8
+        var text by remember { mutableStateOf("") }
+        var warningState by remember { mutableStateOf(false) }
+
+        warningState = text.length > maxLength
+
+        TnTLabeledTextFieldForCode(
+            title = "제목",
+            value = text,
+            onValueChange = { text = it },
+            placeholder = "내용을 입력해주세요",
+            showWarning = warningState,
+            isSingleLine = true,
+            isRequired = false,
+            warningMessage = "인증되었습니다",
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(10.dp),
---
+++ feature/connect/src/main/java/co/kr/tnt/connect/TraineeConnectScreen.kt
@@ -0,0 +1,108 @@
+package co.kr.tnt.connect
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import co.kr.tnt.designsystem.component.TnTLabeledTextFieldForCode
+import co.kr.tnt.designsystem.component.TnTTopBarNoBackButton
+import co.kr.tnt.designsystem.component.button.TnTBottomButton
+import co.kr.tnt.designsystem.component.button.TnTTextButton
+import co.kr.tnt.designsystem.component.button.model.ButtonSize
+import co.kr.tnt.designsystem.theme.TnTTheme
+import co.kr.tnt.feature.connect.R
+
+@Composable
+fun TraineeConnectScreen(
+    onSkipClick: () -> Unit,
+    onNextClick: () -> Unit,
+) {
+    val maxLength = 8
+    var text by remember { mutableStateOf("") }
+    val isWarning by remember { derivedStateOf { text.length == maxLength } }
+
+    Scaffold(
+        topBar = {
+            TnTTopBarNoBackButton(
+                title = stringResource(R.string.connect),
+                trailingComponent = {
+                    Text(
+                        text = stringResource(R.string.skip),
+                        color = TnTTheme.colors.neutralColors.Neutral400,
+                        style = TnTTheme.typography.body2Medium,
+                        modifier = Modifier.clickable {
+                            onSkipClick()
+                        },
+                    )
+                },
+            )
+        },
+        containerColor = TnTTheme.colors.commonColors.Common0,
+    ) { innerPadding ->
+        Box(modifier = Modifier.padding(innerPadding)) {
+            Column(modifier = Modifier.fillMaxSize()) {
+                Spacer(Modifier.padding(top = 24.dp))
+                Text(
+                    text = stringResource(R.string.type_invite_code_from_trainer),
+                    color = TnTTheme.colors.neutralColors.Neutral950,
+                    style = TnTTheme.typography.h2,
+                    modifier = Modifier.padding(horizontal = 24.dp)
+                )
+                Spacer(Modifier.padding(top = 48.dp))
+                TnTLabeledTextFieldForCode(
+                    title = stringResource(R.string.my_invite_code),
+                    value = text,
+                    onValueChange = { text = it },
+                    placeholder = stringResource(R.string.please_type_code),
+                    showWarning = isWarning,
+                    isSingleLine = true,
+                    isRequired = false,
+                    warningMessage = "인증되었어요",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(15.dp),
+                    trailingComponent = {
+                        TnTTextButton(
+                            text = "인증하기",
+                            size = ButtonSize.Small,
+                            onClick = { },
+                        )
+                    },
+                )
+            }
+            TnTBottomButton(
+                text = stringResource(R.string.complete),
+                enabled = text.isNotBlank() && !isWarning,
+                onClick = onNextClick,
+                modifier = Modifier.align(Alignment.BottomCenter),
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun TraineeConnectScreenPreview() {
+    TnTTheme {
+        TraineeConnectScreen(
+            onSkipClick = {},
+            onNextClick = {},
+        )
+    }
+}
