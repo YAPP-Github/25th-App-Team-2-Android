9f9485eff873839521558ca05298266358d0b4b5
SeonJeongk <sjkim5621@naver.com>
2025-01-18 09:25:55 +0900

    feat: 홈 화면 이미지 띄우기

--- feature/home/src/main/java/co/kr/tnt/home/HomeScreen.kt
+++ feature/home/src/main/java/co/kr/tnt/home/HomeScreen.kt
@@ -1,27 +1,71 @@
 package co.kr.tnt.home
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import co.kr.tnt.designsystem.theme.TnTTheme
+import co.kr.tnt.feature.home.R
 
 @Composable
 @Suppress("UnusedParameter")
 internal fun HomeRoute(
+    isTrainer: Boolean,
     viewModel: HomeViewModel = hiltViewModel(),
 ) {
-    HomeScreen()
+    HomeScreen(isTrainer = isTrainer)
 }
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(isTrainer: Boolean) {
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-        Text(
-            text = "home",
-            modifier = Modifier.padding(innerPadding),
+        Box(modifier = Modifier.padding(innerPadding)) {
+            if (isTrainer) {
+                TrainerHomeContent()
+            } else {
+                TraineeHomeContent()
+            }
+        }
+    }
+}
+
+@Composable
+fun TrainerHomeContent() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(R.drawable.img_trainer_home_3x),
+            contentDescription = null,
+            contentScale = ContentScale.Fit,
         )
     }
 }
+
+@Composable
+fun TraineeHomeContent() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(R.drawable.img_trainer_home_3x),
+            contentDescription = null,
+            contentScale = ContentScale.Fit,
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun HomeScreenPreview() {
+    TnTTheme {
+        HomeScreen(
+            isTrainer = true
+        )
+    }
+}
+
